54ee6b4ac06d18131287a324b32a662dfcf07138
zhuzichu <coffee.zhu@jollycorp.com>
2019-11-08 14:30:31 +0800

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,11 @@
-include("app", "shared")
+include(":app", ":shared")
 rootProject.name = "NiceHub"
 
-include("mvvm")
-project(":mvvm").projectDir=(File("D:\\AndroidStudioProjects\\mvvm","app"))
+include(":mvvm")
+project(":mvvm").projectDir=(File("../mvvm","app"))
+
+include(":libs")
+project(":libs").projectDir=(File("../libs","app"))
+
+include(":widget")
+project(":widget").projectDir=(File("../widget","app"))
